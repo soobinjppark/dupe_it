8693ccf281b23dd5623b7d199d98a6c7cade34e7
kathyz08 <zihan.zhang.1@gmail.com>
2020-01-26 14:57:28 -0600

    Homepage Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'login.dart';
-=======
-import './screens/collection_overview.dart'; 
->>>>>>> d9e4740d50754247fa500a5ff3444648ba5244ed
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-<<<<<<< HEAD
 
   @override
   Widget build(BuildContext context) {
@@ -18,16 +13,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
       ),
       home: Login(),
-=======
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CollectionOverviewScreen(),
->>>>>>> d9e4740d50754247fa500a5ff3444648ba5244ed
     );
   }
 }
