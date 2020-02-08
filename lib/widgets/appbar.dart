9de4303a1bd0db36bb89da89fbd7e341da7c97a8
kathyz08 <zihan.zhang.1@gmail.com>
2020-02-08 15:03:19 -0600

    appbar update

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 AppBar appBarBase({String title}) {
   return AppBar(
-    title: Text(title)
+    title: Text(title, style: TextStyle(color: Colors.white)) 
   );
 }
